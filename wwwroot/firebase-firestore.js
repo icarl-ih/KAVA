f0be6702dfefe4a373dacd3cd6fa8c971710b807
icarl-ih <carlos.ih.developer@gmail.com>
2025-08-08 16:02:23 -0600

    Add files via upload

---
+++ wwwroot/firebase-firestore.js
@@ -0,0 +1,22 @@
+﻿window.firestoreInterop = {
+    getUsuarios: async function () {
+        const snapshot = await db.collection("usuarios").get();
+        return snapshot.docs.map(doc => doc.data());
+    },
+
+    getUltimoIdEmpleado: async function () {
+        const snapshot = await db.collection("usuarios")
+            .orderBy("Id_Empleado", "desc")
+            .limit(1)
+            .get();
+
+        if (snapshot.empty) return 1;
+        return snapshot.docs[0].data().Id_Empleado + 1;
+    },
+
+    addUsuario: async function (usuario) {
+        return db.collection("usuarios").add(usuario)
+            .then(() => "OK")
+            .catch(err => err.message);
+    }
+};
---
+++ wwwroot/firebase-init.js
@@ -0,0 +1,35 @@
+﻿
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeFFDCjpHJZUQfV0y0LgIlTKEByAqYwmo",
+    authDomain: "kava-d52c5.firebaseapp.com",
+    projectId: "kava-d52c5",
+    storageBucket: "kava-d52c5.firebasestorage.app",
+    messagingSenderId: "984784531211",
+    appId: "1:984784531211:web:44973e263ca792f64c3c0e",
+    measurementId: "G-K0T14PB4GL"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+window.firestoreInterop = {
+    addUsuario: function (usuario) {
+        return db.collection("Usuarios").add(usuario)
+            .then(() => "OK")
+            .catch(err => err.message);
+    }, getUsuarios: function () {
+        return db.collection("Usuarios").get()
+            .then(snapshot => {
+                const lista = [];
+                snapshot.forEach(doc => {
+                    const data = doc.data();
+                    data.id = doc.id;
+                    lista.push(data);
+                });
+                return lista;
+            })
+            .catch(err => { throw err.message; });
+    }
+
+};
+
