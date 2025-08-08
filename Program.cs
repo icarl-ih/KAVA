60a23e4d3cdd0362614d4e201285de2c2bf1d873
icarl-ih <carlos.ih.developer@gmail.com>
2025-08-08 15:56:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SyncfusionBlazorApp1.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using SyncfusionBlazorApp1;
+using SyncfusionBlazorApp1.lib;
+
+
+
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXdeeHVURmNfVkd0XkJWYEk=");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMemoryCache();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddSingleton<WordService>();
+            builder.Services.AddSingleton<PdfService>();
+            builder.Services.AddSingleton<ExcelService>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<UsuarioService>();
+
+await builder.Build().RunAsync();
